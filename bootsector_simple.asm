88f8f61a26898beccbf0149facb8661925b32edb
Your Name <you@example.com>
2025-10-03 16:06:43 +0200

    x

--- bootsector_simple.asm
+++ bootsector_simple.asm
@@ -2,8 +2,9 @@
 loop:   ; this is a label, like goto in c
     jmp loop
 
-; fill with 510 zeros minus the size of the previous code
+; fill with 510 zeros minus the size of the previous code since the bootsector
+; must be exactly 512 bytes, and we need 2 bytes at the end for the magic number
 times 510-($-$$) db 0
-; magic number
+; boot signature that BIOS looks for (magic number)
 dw 0xaa55
 
